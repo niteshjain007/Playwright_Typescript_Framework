c0a28e5af39ec4357b8d787ea5dd4017357b4bb1
NITESH KUMAR JAIN <nitesh.iiitm@gmail.com>
2025-03-23 10:44:00 +0530

    Add files via upload

---
+++ src/apiTests/apiTest.spec.ts
@@ -0,0 +1,63 @@
+import { test, expect } from "@playwright/test";
+
+test("API test with existing context", async ({ page }) => {
+  const context = page.request;
+
+  const response = await (await context.get("/api/users?page=2")).json();
+  console.log(response);
+  expect(response).toHaveProperty("page");
+  expect(response).toHaveProperty("per_page");
+ 
+  // Assert data type and value for each property
+  expect(typeof response.page).toBe("number");
+  expect(response.page).toEqual(2);
+
+  expect(typeof response.per_page).toBe("number");
+  expect(response.per_page).toEqual(6);
+
+  expect(Array.isArray(response.data)).toBe(true);
+  expect(response.data.length).toEqual(6);
+
+  expect(typeof response.support).toBe("object");
+  expect(typeof response.support.url).toBe("string");
+  expect(typeof response.support.text).toBe("string");
+
+  // Assert properties of each object in the "data" array
+  for (const item of response.data) {
+    expect(typeof item.id).toBe("number");
+    expect(typeof item.email).toBe("string");
+    expect(typeof item.first_name).toBe("string");
+    expect(typeof item.last_name).toBe("string");
+    expect(typeof item.avatar).toBe("string");
+  }
+});
+
+test("API test with new context", async ({ playwright }) => {
+  const apirequest = playwright.request;
+
+  const newcontext = await apirequest.newContext({
+    baseURL: "https://cat-fact.herokuapp.com",
+  });
+
+  
+  const apiResponse = await newcontext.get("/facts/");
+  const apiResponseJson = await apiResponse.json();
+  for (const obj of apiResponseJson) {
+    // Assert properties of each object
+    expect(obj).toHaveProperty("status44");
+    expect(obj).toHaveProperty("_id");
+  
+    // Specific assertions for nested properties
+    expect(obj.status).toHaveProperty("verified");
+    expect(obj.status).toHaveProperty("sentCount");
+
+    // Example assertions for specific values
+    expect(obj.status.verified).toBe(true);
+    expect(obj._id).toMatch(/^\w{24}$/); // Matches ObjectId format
+    expect(obj.user).toMatch(/^\w{24}$/); // Matches ObjectId format
+    expect(obj.text).toContain("cat");
+    expect(obj.source).toBe("user");
+    expect(obj.deleted).toBe(false);
+  }
+});
+
--- src/fixtures/LoginFixture.ts
+++ src/fixtures/LoginFixture.ts
@@ -2,12 +2,10 @@ import { test as base, expect as defaultExpect } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import AllProductPage from "../pages/AllProductPage";
-//import { decrypt } from "../utils/CryptojsUtil";
 
 type UIPages = {
     allProductPageObj: AllProductPage;
 };
-
 export const expect = defaultExpect;
 // Define a custom fixture with page
 export const test = base.extend<UIPages>({
@@ -15,16 +13,16 @@ export const test = base.extend<UIPages>({
 
  const homePageObj = new HomePage(page);
     await homePageObj.NavigateToHomePage();
-page.waitForTimeout(2000);
+    await page.waitForTimeout(2000);
     await homePageObj.clickOnSignIn();
-    page.waitForTimeout(2000);
+    await page.waitForTimeout(2000);
 
   const loginPageObj = new LoginPage(page);
    await loginPageObj.fillEmailValue();
    await  loginPageObj.fillPAsswordValue();
- const  allProductPageObj =loginPageObj.clickOnLoginBtn();
-    await  page.waitForTimeout(2000);
-   
+   const allProductPageObj =await loginPageObj.clickOnLoginBtn();
+   await page.waitForTimeout(2000);
+
     await use(allProductPageObj);
   },
 });
